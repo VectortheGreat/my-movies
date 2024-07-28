1e9ed27987f6fc52995c274bf5acc361e61851bc
Kadir <84664117+VectortheGreat@users.noreply.github.com>
2024-07-29 01:07:03 +0300

    chore: Add react-native-reanimated-carousel dependency and remove unused components

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -2,68 +2,64 @@ import { router } from "expo-router"
 import { Image, ScrollView, StyleSheet, View } from "react-native"
 import { Button, Text } from "react-native-paper"
 import { SafeAreaView } from "react-native-safe-area-context"
-
+import { ImageSwiper } from "../../components/image_swiper"
 export default function HomeScreen() {
   const hideSiteMap = true
   return (
     <SafeAreaView style={{ backgroundColor: "#fff" }}>
       <ScrollView>
+        <Button
+          mode="contained"
+          onPress={() => router.push("_sitemap")}
+          style={{ display: hideSiteMap ? "none" : "flex" }}
+        >
+          Site Map
+        </Button>
+        <Text style={styles.title}>My Movies</Text>
         <View>
-          <Button
-            mode="contained"
-            onPress={() => router.push("_sitemap")}
-            style={{ display: hideSiteMap ? "none" : "flex" }}
-          >
-            Site Map
-          </Button>
-          <Text style={styles.title}>My Movies</Text>
-          <View style={{ padding: 20 }}>
-            <Image
-              src="https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg"
-              style={{
-                width: "100%",
-                height: 200,
-                objectFit: "cover",
-                borderRadius: 20
-              }}
-            />
-          </View>
-          <View style={{ padding: 20 }}>
-            <Text>Trending</Text>
-            <Image
-              src="https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg"
-              style={{
-                width: "100%",
-                height: 200,
-                objectFit: "cover",
-                borderRadius: 20
-              }}
-            />
-          </View>
-          <View style={{ padding: 20 }}>
-            <Text>Discover</Text>
-            <Image
-              src="https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg"
-              style={{
-                width: "100%",
-                height: 200,
-                objectFit: "cover",
-                borderRadius: 20
-              }}
-            />
-          </View>
-          <View style={{ padding: 20 }}>
-            <Text>Discover - Local</Text>
-            <Image
-              src="https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg"
-              style={{
-                width: "100%",
-                height: 200,
-                objectFit: "cover",
-                borderRadius: 20
-              }}
-            />
-          </View>
+          <Image
+            src="https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg"
+            style={{
+              width: "100%",
+              height: 200,
+              objectFit: "cover",
+              borderRadius: 20
+            }}
+          />
+        </View>
+        <View>
+          <Text>Trending</Text>
+
+          <ImageSwiper
+            autoplay
+            images={[
+              "https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg",
+              "https://wallpapers.com/images/featured/movie-background-4saldhgir0h87q13.jpg",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5tOGp6thPMtXJmpwqlHbZlBms3w-jJrVGfQ&s"
+            ]}
+          />
+        </View>
+        <View>
+          <Text>Discover</Text>
+          <ImageSwiper
+            showButtons
+            showPagination
+            images={[
+              "https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg",
+              "https://wallpapers.com/images/featured/movie-background-4saldhgir0h87q13.jpg",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5tOGp6thPMtXJmpwqlHbZlBms3w-jJrVGfQ&s"
+            ]}
+          />
+        </View>
+        <View>
+          <Text>Discover - Local</Text>
+          <ImageSwiper
+            images={[
+              "https://e1.pxfuel.com/desktop-wallpaper/485/771/desktop-wallpaper-hollywood-movie-group-live-action-movies.jpg",
+              "https://wallpapers.com/images/featured/movie-background-4saldhgir0h87q13.jpg",
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5tOGp6thPMtXJmpwqlHbZlBms3w-jJrVGfQ&s"
+            ]}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
